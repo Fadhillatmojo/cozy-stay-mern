7598ad917b1d02f49dfb06db19f713a81875c890
User <fadhillatmojo@gmail.com>
2024-01-15 20:43:27 +0700

    patch: fade reveal from left with react reveal

--- src/parts/IconSection.jsx
+++ src/parts/IconSection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import bagIcon from '../assets/icon/bagicon.svg'
 import cameraIcon from '../assets/icon/cameraicon.svg'
 import locationIcon from '../assets/icon/locationicon.svg'
+import {Fade} from 'react-reveal'
 
 export default function IconSection() {
 	const iconItems = [
@@ -11,25 +12,28 @@ export default function IconSection() {
 	];
 
 	return (
-		<section className='bg-white-secondaryWhite w-full px-2 flex justify-center mt-10 md:mt-12'>
+		<section className='bg-white-secondaryWhite w-full px-2 flex justify-center mt-10 md:mt-12 py-10'>
 			<div className="max-w-screen-lg flex md:flex-row">
-				<div className="grid grid-cols-1 md:grid-cols-3">
-					{
-						iconItems.map(item => 
-						<div key={item.id} className="flex flex-col md:flex-row justify-center md:justify-start items-center">
-							<img src={item.icon} alt="icon" className='md:w-1/2'/>
-							<div className="flex flex-col text-center md:text-left -mt-6 md:-mt-0 md:-ms-2 lg:-ms-3">
-								<h1 className='text-3xl font-medium'>
-									{item.amount}
-								</h1>
-								<p className='text-xl font-medium'>
-									{item.desc}
-								</p>
+				<Fade left>
+					<div className="grid grid-cols-1 md:grid-cols-3">
+						{
+							iconItems.map(item => 
+							<div key={item.id} className="flex flex-col md:flex-row justify-center md:justify-start items-center">
+								<img src={item.icon} alt="icon" className='md:w-1/2'/>
+								<div className="flex flex-col text-center md:text-left -mt-6 md:-mt-0 md:-ms-2 lg:-ms-3">
+									<h1 className='text-3xl font-medium'>
+										{item.amount}
+									</h1>
+									<p className='text-xl font-medium'>
+										{item.desc}
+									</p>
+								</div>
 							</div>
-						</div>
-						)
-					}
-				</div>
+							)
+						}
+					</div>
+				</Fade>
+				
 
 			</div>
 		</section>
